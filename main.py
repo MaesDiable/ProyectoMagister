987591b0536fa215f9241e57b5ba7e4e60543518
MaesDiable <alejandro.cerpa@mayor.cl>
2021-04-04 19:35:22 -0400

    Modificación de lectura

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import codecs
 from liwc import Liwc
 
 #Hay que sacar todos los que no tengan temas de interacción social y
-lwicDicctionary = Liwc("dataSource/Spanish_LIWC2007_Dictionary.dic")
+#lwic = Liwc("dataSource/Spanish_LIWC2007_Dictionary.dic")
 
-print(lwicDicctionary.search("crear"))
+#print(lwic.search("crear"))
+#print(lwic.categories)
 
 
+f = codecs.open("dataSource/Spanish_LIWC2007_Dictionary.dic","r","utf-8")
+print(f.read())
